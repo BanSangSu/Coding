e419934aa14dd1b91d57f77060796d9198afc9df
Ban <asdd3525@nate.com>
2023-05-24 06:36:51 +0900

    BOJ 10845 Python

---
+++ Ban/BOJ/data_structure/[BOJ[10845.py
@@ -0,0 +1,31 @@
+'''
+    https://www.acmicpc.net/problem/10845
+'''
+import sys; input_line = sys.stdin.readline
+from collections import deque
+
+n_commands = int(input_line())
+deq = deque()
+
+i = n_commands
+
+
+while 0 < i:
+    command = input_line().split()
+    cmd = command[0]
+    if cmd == "push":
+        deq.append(int(command[1]))
+    elif cmd == "pop":
+        print(deq.popleft()) if deq else print(-1)
+    elif cmd == "size":
+        print(len(deq))
+    elif cmd == "empty":
+        print(0) if deq else print(1)
+    elif cmd == "front":
+        print(deq[0]) if deq else print(-1)
+    elif cmd == "back":
+        print(deq[-1]) if deq else print(-1)
+    else:
+        print("Wrong command.")
+        i += 1
+    i -= 1
