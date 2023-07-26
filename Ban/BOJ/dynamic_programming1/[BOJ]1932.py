d6bda7c81bdbf67cda2cde6793a7d910c2ca6214
Ban <asdd3525@nate.com>
2023-07-26 09:20:34 +0900

    BOJ 1932 Python

---
+++ Ban/BOJ/dynamic_programming1/[BOJ]1932.py
@@ -0,0 +1,25 @@
+'''
+    https://www.acmicpc.net/problem/1932
+
+    Dynamic Programming    
+'''
+import sys; read_line = lambda: sys.stdin.readline().rstrip()
+
+# sys.stdin = open("data.txt", 'r')
+
+n = int(read_line())
+
+triangle = []
+for _ in range(n):
+    triangle.append(list(map(int, read_line().split())))
+
+for i in range(1, n):
+    # for j in range(len(triangle[i])):
+    for j in range(i+1):
+        if j == 0:
+            triangle[i][j] += triangle[i-1][j]
+        elif j == i:
+            triangle[i][j] += triangle[i-1][j-1]
+        else:
+            triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+print(max(triangle[n-1]))
