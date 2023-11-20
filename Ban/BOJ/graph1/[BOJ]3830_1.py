8c000938fc39e950557e4e5c21ff44934718da4c
BanSangSu <asdd3525@nate.com>
2023-11-21 01:17:25 +0900

    BOJ 3830_1 Python

--- Ban/BOJ/graph1/[BOJ]3830_1.py
+++ Ban/BOJ/graph1/[BOJ]3830_1.py
@@ -1,7 +1,5 @@
 '''
     https://www.acmicpc.net/problem/3830
-
-    Union-Find
 '''
 import sys; read_line = lambda: sys.stdin.readline().rstrip()
 sys.setrecursionlimit(10**6)
@@ -9,19 +7,14 @@ sys.setrecursionlimit(10**6)
 # sys.stdin = open("data.txt", 'r')
 
 def find(x, parent, weights):
-    if x != parent[x]:
-        # weights[x] = weights[x] + weights[parent[x]]
-        # and then update parent of x(parent[x]) to find(parent[x])~~.
-        p_find = find(parent[x], parent, weights)
-        weights[x] += weights[parent[x]]
-        parent[x] = p_find
+    if parent[x] != x:
+        p_find = find(parent[x], parent, weights)    
+        weights[x] += weights[parent[x]]    
+        parent[x] = p_find    
     return parent[x]
-
-def union(a, b, w, parent, weights):
-    a_find = find(a, parent, weights)
-    b_find = find(b, parent, weights)
-
-    if a_find != b_find: 
+    
+def union(a, b, a_find, b_find, w, parent, weights):
+    if a_find != b_find:
         parent[b_find] = a_find
         weights[b_find] = weights[a] - weights[b] + w
 
@@ -29,18 +22,18 @@ while True:
     N, M = map(int, read_line().split())
     if [N, M] == [0, 0]:
         break
-
+    
     weights = [0 for _ in range(N+1)]
     parent = [i for i in range(N+1)]
     for i in range(M):
         tmp = read_line().split()
+        values = [*map(int, tmp[1:])]
+        a, b = values[0], values[1]
+        a_find = find(a, parent, weights)
+        b_find = find(b, parent, weights)
         if tmp[0] == '!':
-            a, b, w = map(int, tmp[1:]) 
-            union(a, b, w, parent, weights)
+            union(a, b, a_find, b_find, values[2], parent, weights)
         elif tmp[0] == '?':
-            a, b = map(int, tmp[1:])
-            a_find = find(a, parent, weights)
-            b_find = find(b, parent, weights)
             if a_find == b_find:
                 print(weights[b] - weights[a])
             else:
