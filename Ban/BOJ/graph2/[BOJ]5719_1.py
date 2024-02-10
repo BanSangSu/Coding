32509d6fe0956abca8a7ec808c8540c5d0e02dc7
BanSangSu <asdd3525@nate.com>
2024-02-10 23:26:25 +0900

    BOJ 5719_1 Python

--- Ban/BOJ/graph2/[BOJ]5719_1.py
+++ Ban/BOJ/graph2/[BOJ]5719_1.py
@@ -6,9 +6,44 @@
 import sys; read_line = lambda: sys.stdin.readline().rstrip()
 import heapq
 
-sys.stdin = open("data.txt", 'r')
+# sys.stdin = open("data.txt", 'r')
 
 INF = sys.maxsize
+
+def dijkstra_1(dist, adj, check, v):
+    dist[v] = 0
+    hq = []
+    heapq.heappush(hq, (v, 0))
+
+    while hq:
+        cur_node, cur_dist = heapq.heappop(hq)
+
+        if dist[cur_node] < cur_dist:
+            continue
+
+        for (next_node, next_dist) in adj[cur_node]:
+            distance = cur_dist + next_dist
+            if distance < dist[next_node] and check[cur_node][next_node] == False:
+                dist[next_node] = distance
+                heapq.heappush(hq, (next_node, distance))
+
+def dijkstra_2(dist, reverse_adj, check, s, v):
+    hq = []
+    heapq.heappush(hq, (v, dist[v]))
+
+    while hq:
+        cur_node, cur_dist = heapq.heappop(hq)
+
+        if cur_node == s:
+            continue
+
+        for (past_node, past_dist) in reverse_adj[cur_node]:
+            if check[past_node][cur_node] == True:
+                continue
+
+            if dist[past_node] == dist[cur_node] - past_dist:
+                check[past_node][cur_node] = True
+                heapq.heappush(hq, (past_node, dist[past_node]))
 
 while True:
     N, M = map(int, read_line().split())
