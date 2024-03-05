8844c72b8d143e158259032281b7a0c1c6f913f2
BanSangSu <asdd3525@nate.com>
2024-03-06 08:49:40 +0900

    BOJ 7579_1 Python in progress because of exam.

---
+++ Ban/BOJ/dynamic_programming2/[BOJ]7579_1.py
@@ -0,0 +1,10 @@
+'''
+    https://www.acmicpc.net/problem/7579
+
+    Knapsack problem (Dynamic programming)
+'''
+import sys; read_line = lambda: sys.stdin.readline().rstrip()
+
+sys.stdin = open("data.txt", 'r')
+
+N, M = map(int, read_line().split())
