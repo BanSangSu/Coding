bd50320da3cf57d7456132014bd92eee53a6bc1d
BanSangSu <asdd3525@nate.com>
2024-03-11 23:15:48 +0900

    BOJ 5582_1 Python

--- Ban/BOJ/dynamic_programming2/[BOJ]5582_1.py
+++ Ban/BOJ/dynamic_programming2/[BOJ]5582_1.py
@@ -2,5 +2,18 @@
     https://www.acmicpc.net/problem/5582
 '''
 import sys; read_line = lambda: sys.stdin.readline().rstrip()
+from collections import deque
 
-sys.stdin = open("data.txt", 'r')
+# sys.stdin = open("data.txt", 'r')
+
+s1 = read_line()
+s2 = read_line()
+
+dq = deque()
+ans = 0
+for i in s1:
+    dq.append(i)
+    while "".join(dq) not in s2:
+        dq.popleft()
+    ans = max(ans, len(dq))
+print(ans)
